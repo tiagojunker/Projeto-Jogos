b97ab1ec2a70325bfba38ca5eb57b5674e32cfaa
Tiago Junker <92689798+tiagojunker@users.noreply.github.com>
2022-04-17 03:20:17 -0300

    "Projeto SpaceShooter Funcionando porém incompleto"
    Desta vez algumas funcionalidades ficaram diferentes das vistas em aula

--- shooter.js
+++ shooter.js
@@ -1,7 +1,9 @@
 const yourShip = document.getElementsByClassName('player-shooter')[0];
 const playArea = document.getElementById('main-play-area');
 const objetosImg = ['shooter/machine.png', 'shooter/planet.png', 'shooter/fire-brock.png'];
-
+const instructionsText = document.getElementsByClassName('game-instructions')[0];
+const startButton = document.getElementsByClassName('start-button')[0];
+let objectInterval;
 
 //Função movimento e disparo
 function flyShip(event) {
@@ -56,13 +58,22 @@ function createLaserElement() {
     newLaser.src = 'shooter/shot.png';  //adiciona o source a TAG img
     newLaser.classList.add('laser') ;  //adiciona uma classe a TAG img
     newLaser.style.left = `${xPosition}px`;   //posiciona o laser
-    newLaser.style.top = `${yPosition - 10}px`;  //posiciona o laser
+    newLaser.style.top = `${yPosition - 15}px`;  //posiciona o laser
     return newLaser;
 }
 
 function moveLaser(laser) {
     let laserInterval = setInterval(() => {
         let xPosition = parseInt(laser.style.left);
+        let objects = document.querySelectorAll('.obj')
+
+        objects.forEach((object) => {  //comparando se objeto foi atingido, se sim troca o src da img
+            if(checkLaserCollision(laser, object)) {
+                object.src = 'shooter/explosion.png';
+                object.classList.remove('obj')
+                object.classList.add('dead-alien');
+            }
+        })
         
         if( xPosition === 580) {
             laser.remove();
@@ -75,16 +86,88 @@ function moveLaser(laser) {
 //função para criar inimigos(objetos) aleatórios
 function createObjects() {
     let newObject = document.createElement('img');
-    let objectSprite = objetosImg[Math.floor(math.random() * objetosImg.length)]; //sorteio de imagem
+    let objectSprite = objetosImg[Math.floor(Math.random() * objetosImg.length)]; //sorteio de imagem
     newObject.src = objectSprite;
     newObject.classList.add('obj');
     newObject.classList.add('obj-transition');
     newObject.style.left = `370px`;
-    newObject.style.top = `${math.floor(math.random() * 330) + 30}`;
+    newObject.style.top = `${Math.floor(Math.random() * 330) + 30}px`;
     playArea.appendChild(newObject);
-    moveObject();
+    moveObject(newObject);
+}
 
+//função para movimentar os inimigos
+
+function moveObject(object) {
+    let moveObjectInterval = setInterval(() => {
+        console.log(object)
+        let xPosition = parseInt(window.getComputedStyle(object).getPropertyValue('left'));
+        if(xPosition <= 50) {
+            if(Array.from(object.classList).includes('dead-alien')) {
+                object.remove();
+            } else {
+                gameOver();
+            } 
+        } else {
+            object.style.left = `${xPosition - 1}px`
+        }
+    },20);
+}  
+
+//função para colisão
+function checkLaserCollision(laser, object) {
+    let laserTop = parseInt(laser.style.top);
+    let laserLeft = parseInt(laser.style.left);
+    let laserBottom = laserTop - 20;
+
+    let objectTop = parseInt(object.style.top);
+    let objectLeft = parseInt(object.style.left);
+    let objectBottom = objectTop - 30;
+
+    if(laserLeft != 340 && laserLeft + 60 >= objectLeft) {
+        if(laserTop <= objectTop && laserTop >= objectBottom) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false
+    }
 }
 
 
-window.addEventListener('keydown', flyShip); //função que ativa o teclado
+//inicia o Jogo
+
+startButton.addEventListener('click', (event) => {
+    playGame();
+})
+
+function playGame() {
+    startButton.style.display = 'none';
+    instructionsText.style.display = 'none';
+    window.addEventListener('keydown', flyShip);
+
+    objectInterval = setInterval(() => {
+        createObjects();
+    },3000)
+}
+
+//função de game over
+
+function gameOver() {
+    window.removeEventListener['keydown', flyShip]
+    clearInterval(objectInterval);
+    let objects = document.querySelectorAll('.obj');
+    objects.forEach((object) => object.remove());
+    let lasers = document.querySelectorAll('.laser');
+    lasers.forEach((laser) => laser.remove());   
+    yourShip.style.top = '350px';
+    startButton.style.display = 'block';
+    startButton.style.justfyContent = 'center';
+    startButton.style.alignItems = 'center';
+    startButton.style.padding = '15px 0'
+    instructionsText.style.display = 'block';
+    instructionsText.style.justfyContent = 'center';
+    instructionsText.style.alignItems = 'center';
+    instructionsText.style.textAlign = 'center';
+}
